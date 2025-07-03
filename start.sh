a3d1af2c2ceeb9093053900bd1d303f1d00eccb4
Ishatgrepo <dmcomdot@gmail.com>
2025-07-03 20:38:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all=true --rpc-listen-port=6800 >> /usr/src/app/aria2c.log 2>&1 &
+qbittorrent-nox --webui-port=8090 --daemon &
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all=true --rpc-listen-port=6800 
 source mltbenv/bin/activate
 python3 update.py
 python3 -m bot
