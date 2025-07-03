b684711f22f7fa7551ddb8a18d1fab31c8a1fa9a
Ishatgrepo <dmcomdot@gmail.com>
2025-07-03 20:34:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all=true --rpc-listen-port=6800 >> /usr/src/app/aria2c.log 2>&1 &
 source mltbenv/bin/activate
 python3 update.py
 python3 -m bot
